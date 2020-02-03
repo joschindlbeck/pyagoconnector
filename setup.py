e98c1872ea99d6ab8badf3cbf649bb98c23020a1
joschindlbeck <31587281+joschindlbeck@users.noreply.github.com>
2020-02-03 17:30:33 +0100

    prepare for pypi

--- setup.py
+++ setup.py
@@ -8,11 +8,10 @@ setup(
   author = 'joschindlbeck',                   # Type in your name
   author_email = 'joschindlbeck@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/joschindlbeck/pyagoconnector',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/joschindlbeck/pyagoconnector/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['AGOpenGPS', 'Connector', 'Noobish'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
